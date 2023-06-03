0b2452f88ba581153ddf1acc1ce816eca9aefe6e
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2023-06-02 21:49:43 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,7 +8,6 @@ function start() {
     ms = Math.floor((timer - Math.floor(timer)) * 100);
     sec = Math.floor(timer) - Math.floor(timer / 60) * 60;
     min = Math.floor(timer / 60);
-    hr = Math.floor(timer);
     document.getElementById('millisecs').innerHTML = ms < 10 ? "0" + ms.toString() : ms;
     document.getElementById('secs').innerHTML = sec < 10 ? "0" + sec.toString() : sec;
     document.getElementById('mins').innerHTML = min < 10 ? "0" + min.toString() : min;
